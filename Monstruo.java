15c82eb37407e127bb370d2f8063d64f55677d21
Oscar Gutiérrez Castillo <Ator97@MacBook-Pro-de-Oscar.local>
2016-10-11 08:56:55 -0500

    Agregando el constructor del monstruo

--- Monstruo.java
+++ Monstruo.java
@@ -1,22 +1,31 @@
 public  abstract class Monstruos{
 	
-	float hp;
-	String apodo;
-	int nivel;
-	float ataque;
-	float defensa;
-	float velocidad;
-	String estado;
+	protected float hp ;
+	protected String apodo;
+	protected int nivel;
+	protected float ataque;
+	protected float defensa;
+	protected float velocidad;
+	protected String estado = 'Ok';
 
-	public abstract void recibirDanio(float ataque);
-	public abstract void recibirAtaque(pocima);
-	public abstract void recibirDefensa();
-	public abstract void getAtaque();
-	public abstract void getDefensa();
-	public abstract void getVelocidad();
-	public abstract void setAtaque();
-	public abstract void setDefensa();
-	public abstract void setVelocidad();
+	Monstruos (int nivel, String apodo){
+		this.ataque = nivel * 15;
+		this.defensa = nivel * 10;
+		this.velocidad = nivel * 10;
+		this.hp = nivel  *15;
+		this.apodo = apodo;
+	}
+	public  void recibirDanio(float ataque){
+		this.hp = hp - ataque * (1/3)* defensa;
+	}
+	public  void recibirAtaque(Pocima p);
+	public  void recibirDefensa();
+	public  void getAtaque();
+	public  void getDefensa();
+	public  void getVelocidad();
+	public  void setAtaque();
+	public  void setDefensa();
+	public  void setVelocidad();
 	public abstract void multiplicadorElemental();
 	public abstract void ataque1();
 	public abstract void ataque2();
