75c06e6fea39129ed799c60e5a54e026e6ea5c24
Ator96 <oscargutcas9705@gmail.com>
2016-10-12 12:47:14 -0500

    Con documentación

--- Monstruo.java
+++ Monstruo.java
@@ -3,6 +3,8 @@ package pokeprebe;
 /**
 *@author Oscar Gutiérrez Castillo
 *@version 1.0.0.
+* La clase Monstruo genera la plantilla básica de la cual emanarán todos los tipos  y 
+* monstruos específicos.
 */
 
 public abstract class Monstruo{
@@ -17,6 +19,10 @@ public abstract class Monstruo{
 	/**
 	*@param nivel nivel del monstruo dado por el usuario
 	*@param apodo apodo del monstruo dado por el usuario
+        * El metodo define los status del mosntruo
+        * Los parametros que recibe on gracias a la herencia 
+        * dada por los cuatro tipos elementales.
+        * Para este método construcor, se solicita saber el nivel y el apodo que identificará a futuro al monstruo.
 	*/
         @SuppressWarnings("OverridableMethodCallInConstructor")
 	Monstruo (int nivel, String apodo){
@@ -29,6 +35,9 @@ public abstract class Monstruo{
 
 	/**
 	*@param nivel nivel del mosntruo dado por el usuario
+        * El método define los estatus del monstruo
+        * Este constructor solo recibe el nivel del monstruo
+        * El apodo será omitido y se dará un nombre default.
 	*/
         @SuppressWarnings("OverridableMethodCallInConstructor")
 	Monstruo (int nivel){
@@ -39,7 +48,8 @@ public abstract class Monstruo{
 	}
 	/**
 	*@param ataque Danio recibido al monstruo
-     * @return 
+        *@return estado se regresa una cadena cuando el monstruo ha sido derrotado
+        * El método  actualiza la vida del mosntruo acorde al numero de daño que se le apliqué
 	*/
 	public  String recibirDanio(float ataque){
 		this.hp = this.hp - ataque;
@@ -53,6 +63,7 @@ public abstract class Monstruo{
 	/**
 	*@param p pocima de aumento de vida recibida por su dueño
         * @return Comentario sobre el estado
+        * El método actualia la vida del monstruo aumentando la vida acorde a la posión que se le dé
 	*/
 	public  String recibirHp(PocimaVida p){
 		this.hp = this.hp + p.aumento;
@@ -60,6 +71,7 @@ public abstract class Monstruo{
 	}
 	/**
 	*@param p pocima de aumento de defensa recibida por el dueño
+        * Actualiza la cantidad de desensa que posee el monstruo al recibir una pocima
 	*/
 	public  void recibirDefensa(PocimaVida p){
 		this.defensa = this.defensa + p.aumento;
@@ -70,18 +82,36 @@ public abstract class Monstruo{
 	public  float getAtaque(){
 		return this.ataque;
 	}
+	/**
+	*@return ataque Obtenemos la defensa  que posee el monstruo
+	*/
 	public  float getDefensa(){
 		return this.defensa;
 	}
+	/**
+	*@return ataque Obtenemos la vida que posee el monstruo
+	*/
 	public  float getVelocidad(){
 		return this.velocidad;
 	}
+	/**
+        * @param ataque nivel de ataque con el que es inicializado
+        * Obtenemos poder de ataque que posee el monstruo
+	*/
 	public  void setAtaque(float ataque){
 		this.ataque += ataque;
 	}
+	/**
+        * @param defensa nivel de ataque con el que es inicializado
+        * Obtenemos poder de defensa que posee el monstruo
+	*/        
 	public  void setDefensa(float defensa){
 		this.defensa += defensa;
 	}
+	/**
+        *@param velocidad nivel de ataque con el que es inicializado
+        *Obtenemos poder de la velocidad que posee el monstruo
+	*/        
 	public  void setVelocidad(float velocidad){
 		this.velocidad += velocidad;
 
