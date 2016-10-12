ae736b4051bac4363d03a533a230bf7cd53265e6
Ator96 <oscargutcas9705@gmail.com>
2016-10-11 23:59:04 -0500

    Migrado e implementación
    Se ha completado la clase y se ha migrado para uso con paquetes

--- Agua.java
+++ Agua.java
@@ -1,6 +1,57 @@
-public class Agua extends Monstruo{
+package pokeprebe;
 
-	
-	public void multiplicadorElemental()
+/**
+ *
+ * @author Ator97
+ */
+public abstract class Agua extends Monstruo {
 
+    float multiplicador;
+
+    public Agua(int nivel, String apodo) {
+        super(nivel, apodo);
+    }
+
+    /**
+     *
+     * @param nivel
+     */
+    public Agua(int nivel) {
+        super(nivel);
+    }
+
+    /**
+     *
+     * @param m
+     * @return
+     */
+    @Override
+    public float multiplicadorElemental(MonstruoEspecifico m) {
+	if (m instanceof Agua){
+		multiplicador = 0.5f; 
+	}
+	//if (m  instanceof Fuego){
+	//	multiplicador = (float) 2.0; 
+	//}
+	//if (m  instanceof Hierba){
+	//	multiplicador = (float) 0.5; 
+	//}		
+	//if (m  instanceof Electrico){
+	//	multiplicador = (float) 1.0; 
+
+        //}    
+        return 0;
+        
+    }
+    @Override
+    public String ataque1(float multiplicador, MonstruoEspecifico m) {
+	float danio = (getAtaque() -  m.getDefensa() ) * multiplicador;
+	if (danio <= 0){
+		return "parece que no ha habido daño alguno";
+	}
+	m.recibirDanio(danio);    
+        return null;
+    }
+    
+    
 }
